18d4c7b7eda356abaff14815e51c58163edb4780
Paul Lainchbury <paul@lainchbury.com>
2014-02-25 18:37:00 +0000

    Image can be selected, saved and edited.

--- app/models/cheerup.rb
+++ app/models/cheerup.rb
@@ -1,7 +1,7 @@
 class Cheerup < ActiveRecord::Base
   belongs_to :user
   make_flaggable :inappropriate
-  attr_accessible :title, :sub_title, :cheerpoints
+  attr_accessible :title, :sub_title, :cheerpoints, :image
 
   default_scope order("created_at DESC")
 
