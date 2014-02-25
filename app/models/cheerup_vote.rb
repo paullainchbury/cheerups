6eae3763236efea516ae7201962b73e7f8cd0e01
Thomas Martin <Tom@Toms-MacBook-Pro.local>
2014-02-25 19:15:06 +0000

    Everything Tom has done so far

---
+++ app/models/cheerup_vote.rb
@@ -0,0 +1,14 @@
+class CheerupVote < ActiveRecord::Base
+  attr_accessible :cheerup_id, :user_id, :value
+
+  belongs_to :cheerup
+  belongs_to :user
+
+  validates_uniqueness_of :cheerup_id, scope: :user_id
+  validates_inclusion_of :value, in: [1 , -1]
+  validate :ensure_not_author 
+
+  def ensure_not_author
+    errors.add :user_id, "is the author of the cheerup" if cheerup.user_id == user_id
+  end
+end
