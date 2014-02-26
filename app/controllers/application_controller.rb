b7410b078d0c52fb9fd08f4b4571a8dd770b1780
Mark Randhawa <mark.randhawa@gmail.com>
2014-02-26 14:10:06 +0000

    fixing conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-<<<<<<< HEAD
+
   rescue_from CanCan::AccessDenied do |exception|
   redirect_to root_url , alert: "You can't access this page"
   end
-=======
+
   before_filter :define_users
->>>>>>> af3274836e77f3783ae77c7f85f1f093f7385fd0
+
 
   def after_sign_in_path_for(resource)
     new_cheerup_path
